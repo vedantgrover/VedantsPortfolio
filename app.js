548c892e0109e899e609ec8cc0722c526edd58ca
vedantgrover <68160833+vedantgrover@users.noreply.github.com>
2023-03-10 19:20:00 -0800

    Adding a header
    Along with adding a header, I also created some starter code for the footer.

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
 // Imports
 const express = require("express");
+
 const app = express();
 const port = 3000;
 
 // Static Files
 app.use(express.static('public'));
 app.use('/css', express.static(__dirname + "public/css"));
-app.use('/js', express.static(__dirname + "public/js"));
-app.use('/images', express.static(__dirname + "public/images"));
 
 // Set Views
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
 app.get('', (req, res) => {
-    res.render('index', { text: 'This is EJS'});
-})
-
-app.get('/about', (req, res) => {
-    res.render('about', { text: 'About page'});
+    res.render('index');
 });
+
+app.get('/contact', (req, res) => {
+    res.render('contact')
+})
 
 // Listen on port 3000
 app.listen(port, () => console.info(`Listening on port ${port}`));
