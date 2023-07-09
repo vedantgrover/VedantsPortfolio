9bc34c49017b40dec109cec7d3fc0179d91a3a9b
vedantgrover <68160833+vedantgrover@users.noreply.github.com>
2023-07-08 17:38:46 -0700

    Adds contact form to contact page

--- app.js
+++ app.js
@@ -1,30 +1,13 @@
 // Imports
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const nodemailer = require("nodemailer");
+const bodyParser = require("body-parser");
 require('dotenv').config();
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const port = process.env.PORT;
-const databaseSRV = process.env.DATABASE_SRV
-
-const client = new MongoClient(databaseSRV, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict:true,
-        deprecationErrors: true
-    }
-});
-
-async function connect() {
-    try {
-        await client.connect();
-        await client.db("admin").command({ ping: 1});
-        console.log("Pinged deployment\nYou are connected to MongoDB");
-    } finally {
-        await client.close();
-    }
-}
 
 // Static Files
 app.use(express.static('public'));
@@ -36,6 +19,15 @@ app.use('/images', express.static(__dirname + "public/images"));
 app.use(express.static('public'));
 app.use('/js', express.static(__dirname + "public/js"));
 
+// Nodemailer configuration
+const transporter = nodemailer.createTransport({
+    service: 'gmail', // e.g., Gmail, Outlook
+    auth: {
+        user: 'bladedurman@gmail.com',
+        pass: process.env.EMAIL_PASSWORD
+    }
+});
+
 // Set Views
 app.set('views', './views');
 app.set('view engine', 'ejs');
@@ -46,10 +38,41 @@ app.get('', (req, res) => {
 
 app.get('/contact', (req, res) => {
     res.render('contact')
+});
+
+app.post('/contact', (req, res) => {
+    const { firstname, lastname, email, message } = req.body;
+
+    // Process the form data
+    // Create the email content
+    const mailOptions = {
+        from: 'bladedurman@gmail.com',
+        to: 'vedantvgrover@gmail.com',
+        subject: 'New Contact Form Submission',
+        text: `Name: ${firstname} ${lastname}\n
+                Email: ${email}\n
+                Message: ${message}`
+    };
+
+    // Send the email
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error(error);
+            res.status(500).send('Error sending email');
+        } else {
+            console.log('Email sent:', info.response);
+            res.redirect('/thank-you');
+        }
+    });
+
+    // Redirect the user to a thank-you page
+    res.redirect('/thank-you');
+});
+
+app.get("/thank-you", (req, res) => {
+    res.render("thank-you")
 })
 
-// Connecting to MongoDB
-connect().catch(console.dir);
 
 // Listen on port
 app.listen(port, () => console.info(`Listening on port ${port}`));
