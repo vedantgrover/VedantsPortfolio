c59dbb1ddca010adc615982be3048258f5f699a3
vedantgrover <68160833+vedantgrover@users.noreply.github.com>
2023-07-13 17:10:37 -0700

    Fixes Navbar Bug

--- components/navbar.js
+++ components/navbar.js
@@ -1,4 +1,5 @@
-import Logo from "./logo";
+import { forwardRef } from 'react'
+import Logo from './logo'
 import NextLink from 'next/link'
 import {
     Container,
@@ -13,23 +14,31 @@ import {
     MenuButton,
     IconButton,
     useColorModeValue
-} from "@chakra-ui/react"
-import { HamburgerIcon } from "@chakra-ui/icons"
+} from '@chakra-ui/react'
+import { HamburgerIcon } from '@chakra-ui/icons'
 
-const LinkItem = ({ href, path, children }) => {
+const LinkItem = ({ href, path, target, children, ...props }) => {
     const active = path === href
-    const inactiveColor = useColorModeValue('gray200', 'whiteAlpha.900')
+    const inactiveColor = useColorModeValue('gray.800', 'whiteAlpha.900')
     return (
-        <NextLink href={href}>
-            <Link
-                p={2}
-                bg={active ? 'glassTeal' : undefined}
-                color={active ? '#202023' : inactiveColor}>
-                {children}
-            </Link>
-        </NextLink>
+        <Link
+            as={NextLink}
+            href={href}
+            scroll={false}
+            p={2}
+            bg={active ? 'grassTeal' : undefined}
+            color={active ? '#202023' : inactiveColor}
+            target={target}
+            {...props}
+        >
+            {children}
+        </Link>
     )
 }
+
+const MenuLink = forwardRef((props, ref) => (
+    <Link ref={ref} as={NextLink} {...props} />
+))
 
 const Navbar = props => {
     const { path } = props
@@ -40,10 +49,18 @@ const Navbar = props => {
             as="nav"
             w="100%"
             bg={useColorModeValue('#ffffff40', '#20202380')}
-            style={{ backdropFilter: 'blur(10px' }}
-            zIndex={1}
-            {...props}>
-            <Container display="flex" p={2} maxW="container.md" wrap="wrap" align="center" justify="space-between">
+            css={{ backdropFilter: 'blur(10px)' }}
+            zIndex={2}
+            {...props}
+        >
+            <Container
+                display="flex"
+                p={2}
+                maxW="container.md"
+                wrap="wrap"
+                align="center"
+                justify="space-between"
+            >
                 <Flex align="center" mr={5}>
                     <Heading as="h1" size="lg" letterSpacing={'tighter'}>
                         <Logo />
@@ -56,7 +73,8 @@ const Navbar = props => {
                     width={{ base: 'full', md: 'auto' }}
                     alignItems="center"
                     flexGrow={1}
-                    mt={{ base: 4, nmd: 0 }}>
+                    mt={{ base: 4, md: 0 }}
+                >
                     <LinkItem href="/works" path={path}>
                         Works
                     </LinkItem>
@@ -67,21 +85,29 @@ const Navbar = props => {
 
                 <Box flex={1} align="right">
                     <Box ml={2} display={{ base: 'inline-block', md: 'none' }}>
-                        <Menu>
-                            <MenuButton as={IconButton} icon={<HamburgerIcon />} variant="outline" aria-label="Options" />
+                        <Menu isLazy id="navbar-menu">
+                            <MenuButton
+                                as={IconButton}
+                                icon={<HamburgerIcon />}
+                                variant="outline"
+                                aria-label="Options"
+                            />
                             <MenuList>
-                                <NextLink href="/" passHref>
-                                    <MenuItem as={Link}>About</MenuItem>
-                                </NextLink>
-                                <NextLink href="/works" passHref>
-                                    <MenuItem as={Link}>Works</MenuItem>
-                                </NextLink>
-                                <NextLink href="/posts" passHref>
-                                    <MenuItem as={Link}>Posts</MenuItem>
-                                </NextLink>
-                                <NextLink href="https://github.com/vedantgrover" passHref>
-                                    <MenuItem as={Link}>View Code</MenuItem>
-                                </NextLink>
+                                <MenuItem as={MenuLink} href="/">
+                                    About
+                                </MenuItem>
+                                <MenuItem as={MenuLink} href="/works">
+                                    Works
+                                </MenuItem>
+                                <MenuItem as={MenuLink} href="/posts">
+                                    Posts
+                                </MenuItem>
+                                <MenuItem
+                                    as={Link}
+                                    href="https://github.com/vedantgrover"
+                                >
+                                    View Source
+                                </MenuItem>
                             </MenuList>
                         </Menu>
                     </Box>
