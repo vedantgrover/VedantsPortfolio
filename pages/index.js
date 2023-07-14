0a69f6ee05e372f236ed5bc98f64c6e686308202
vedantgrover <68160833+vedantgrover@users.noreply.github.com>
2023-07-13 17:52:35 -0700

    Adds sections

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
-import { Container, Box, Heading, Image, useColorModeValue } from "@chakra-ui/react"
+import { Container, Box, Heading, Image, useColorModeValue, Link } from "@chakra-ui/react"
+import Section from "../components/section"
+import Paragraph from "../components/paragraph"
 
 const Page = () => {
     return (
@@ -12,10 +14,18 @@ const Page = () => {
                     <Heading as="h2" variant="page-title">Vedant Grover</Heading>
                     <p>Aspring Developer</p>
                 </Box>
-                <Box flexShrink={0} mt={{base: 4, md: 0}} ml={{md: 6}} align="center">
-                    <Image borderColor="whiteAlpha.800" borderWidth={2} borderStyle="solid" maxWidth="100px" borderRadius="full" src="/images/me1.jpeg" alt="Profile Image"/>
+                <Box flexShrink={0} mt={{ base: 4, md: 0 }} ml={{ md: 6 }} align="center">
+                    <Image borderColor="whiteAlpha.800" borderWidth={2} borderStyle="solid" maxWidth="100px" borderRadius="full" src="/images/me1.jpeg" alt="Profile Image" />
                 </Box>
             </Box>
+
+            <Section delay={0.1}>
+                <Heading as="h3" variant="section-title">
+                    Work
+                </Heading>
+                <br/>
+                <Paragraph>Hello!</Paragraph>
+            </Section>
         </Container>
     )
 }
