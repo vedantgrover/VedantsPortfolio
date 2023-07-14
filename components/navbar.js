37d7bdab18b0d5eae64ce87fe13b514fdd321607
vedantgrover <68160833+vedantgrover@users.noreply.github.com>
2023-07-13 17:34:54 -0700

    Adds profile picture and dark/light theme toggle

--- components/navbar.js
+++ components/navbar.js
@@ -16,6 +16,7 @@ import {
     useColorModeValue
 } from '@chakra-ui/react'
 import { HamburgerIcon } from '@chakra-ui/icons'
+import ThemeToggleButton from './theme-toggle-button'
 
 const LinkItem = ({ href, path, target, children, ...props }) => {
     const active = path === href
@@ -84,6 +85,7 @@ const Navbar = props => {
                 </Stack>
 
                 <Box flex={1} align="right">
+                    <ThemeToggleButton />
                     <Box ml={2} display={{ base: 'inline-block', md: 'none' }}>
                         <Menu isLazy id="navbar-menu">
                             <MenuButton
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import { Container, Box, Heading } from "@chakra-ui/react"
+import { Container, Box, Heading, Image, useColorModeValue } from "@chakra-ui/react"
 
 const Page = () => {
     return (
         <Container>
-            <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+            <Box borderRadius="lg" bg={useColorModeValue("whiteAlpha.500", "whiteAlpha.200")} p={3} mb={6} align="center">
                 Hello, I&apos;m a full-stack developer based in Seattle
             </Box>
 
@@ -11,6 +11,9 @@ const Page = () => {
                 <Box flexGrow={1}>
                     <Heading as="h2" variant="page-title">Vedant Grover</Heading>
                     <p>Aspring Developer</p>
+                </Box>
+                <Box flexShrink={0} mt={{base: 4, md: 0}} ml={{md: 6}} align="center">
+                    <Image borderColor="whiteAlpha.800" borderWidth={2} borderStyle="solid" maxWidth="100px" borderRadius="full" src="/images/me1.jpeg" alt="Profile Image"/>
                 </Box>
             </Box>
         </Container>
