891ca5cd51748099cd877188b83e6c376224e57a
Vedant Grover <68160833+vedantgrover@users.noreply.github.com>
2023-07-16 15:56:54 -0700

    Revert "Portfolio revision"

--- public/js/script.js
+++
@@ -1,6 +0,0 @@
-const toggleIcon = document.querySelector('.toggle-icon');
-
-toggleIcon.addEventListener('click', () => {
-    toggleIcon.classList.toggle('bx-sun');
-    document.body.classList.toggle('dark-mode')
-});
