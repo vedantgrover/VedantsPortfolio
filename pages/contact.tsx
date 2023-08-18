1e56fd8cb0151010204ccfe1ff11115e447a9037
vedantgrover <68160833+vedantgrover@users.noreply.github.com>
2023-08-17 19:41:25 -0700

    Changes API URL

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -14,7 +14,7 @@ export default function Contact() {
     e.preventDefault();
 
     const response = await fetch(
-      `http://localhost:3000/api/email?email=${email}`,
+      `http://vedantgrover.com/api/email?email=${email}`,
       {
         method: "POST",
         headers: {
