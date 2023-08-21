f5a1238967b1525137a49ab760f57382d9663190
vedantgrover <68160833+vedantgrover@users.noreply.github.com>
2023-08-21 14:37:50 -0700

    Fixes project images not loading

--- components/projectBox.tsx
+++ components/projectBox.tsx
@@ -27,18 +27,14 @@ export default function ProjectBox({
 
   return (
     <li key={title} className="group relative flex flex-col items-start">
-      <div className="relative z-10 flex h-12 w-12 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
-        <Image
-          src={logo}
-          alt="Project Logo"
-          loading="lazy"
-          width={32}
-          height={32}
-          decoding="async"
-          className="h-8 w-8 rounded-full"
-          style={{ color: "transparent" }}
-        />
-      </div>
+      <div
+        className="relative z-10 flex h-12 w-12 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0"
+        style={{
+          backgroundImage: `url(${logo})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      ></div>
       <h2 className="mt-6 text-base font-semibold text-zinc-800 dark:text-zinc-100">
         <div className="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 dark:bg-zinc-800/50 sm:-inset-x-6 sm:rounded-2xl"></div>
         <Link href={link} target="_blank">
