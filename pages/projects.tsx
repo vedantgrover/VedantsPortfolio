2dcefbaed5e1660074ee5575613491401429763e
vedantgrover <68160833+vedantgrover@users.noreply.github.com>
2023-08-22 11:52:55 -0700

    Fixes apotstrophes in project strings

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -7,21 +7,21 @@ export default function Projects() {
       projectName: "Apollo18",
       projectLogo: "/images/projects/apollo-pfp.png",
       description:
-        "Introducing Apollo18, my cool Discord Bot adventure! It&apos;s not just a bot, but my coding journey from APIs to Databases and beyond. I picked up all the tricks while working on it, and when it started impressing folks, I decided to share the love. Now it&apos;s open-source, so anyone can join in and help take Apollo18 to new heights!",
+        "Introducing Apollo18, my cool Discord Bot adventure! It's not just a bot, but my coding journey from APIs to Databases and beyond. I picked up all the tricks while working on it, and when it started impressing folks, I decided to share the love. Now it's open-source, so anyone can join in and help take Apollo18 to new heights!",
       link: "https://github.com/vedantgrover/Apollo18",
     },
     {
       projectName: "Typical Programming",
       projectLogo: "/images/projects/typical-logo.jpeg",
       description:
-        "I built my own programming language from scratch using Python, an adventure that not only introduced me to the language but also deepened my understanding of programming languages&apos; inner workings. This journey ignited my passion for coding and expanded my insights into the fascinating world of programming languages.",
+        "I built my own programming language from scratch using Python, an adventure that not only introduced me to the language but also deepened my understanding of programming languages' inner workings. This journey ignited my passion for coding and expanded my insights into the fascinating world of programming languages.",
       link: "https://github.com/vedantgrover/TypicalProgramming",
     },
     {
       projectName: "BladeCraft",
       projectLogo: "/images/projects/bladeCraft.png",
       description:
-        "Say hello to my Minecraft mod! It&apos;s where my coding journey began – my way of spicing up Minecraft and making it a real blast. While I might&apos;ve taken a break from it, I&apos;m always hungry for new ideas to make it even cooler. It&apos;s like my personal sandbox where the possibilities are endless!",
+        "Say hello to my Minecraft mod! It's where my coding journey began – my way of spicing up Minecraft and making it a real blast. While I might've taken a break from it, I'm always hungry for new ideas to make it even cooler. It's like my personal sandbox where the possibilities are endless!",
       link: "https://github.com/vedantgrover/BladeCraftForge",
     },
     {
