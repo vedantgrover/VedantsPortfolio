f5c88325f764872155889abf0f1158e526b99178
vedantgrover <68160833+vedantgrover@users.noreply.github.com>
2023-08-22 20:29:47 -0700

    Moving viewport to layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,10 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/layouts/main";
-import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
