89d3a42340925dad5dec6947feffe993c1125e39
vedantgrover <68160833+vedantgrover@users.noreply.github.com>
2023-08-22 20:29:47 -0700

    Moving viewport to layout

--- components/layouts/main.tsx
+++ components/layouts/main.tsx
@@ -4,12 +4,17 @@ import Footer from "../footer";
 
 export default function Main({ children }: any) {
   return (
-    <div className="py-4 px-20 bg-white dark:bg-darkModeGray min-h-screen">
-      <Navbar />
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <div className="py-4 px-20 bg-white dark:bg-darkModeGray min-h-screen">
+        <Navbar />
 
-      <div>{children}</div>
+        <div>{children}</div>
 
-      <Footer />
-    </div>
+        <Footer />
+      </div>
+    </>
   );
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,9 +6,6 @@ import Head from "next/head";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
