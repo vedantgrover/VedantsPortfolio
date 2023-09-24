74018fae7ee04dd3052a320b883a935a8a50f68b
vedantgrover <68160833+vedantgrover@users.noreply.github.com>
2023-09-23 19:13:03 -0700

    Changes directory to blogs

--- pages/blogs/[id].tsx
+++ pages/blogs/[id].tsx
@@ -2,7 +2,8 @@ import { getPostData, getAllPostIds } from "@/lib/posts";
 import Head from "next/head";
 import Date from "../../components/date";
 import { GetStaticProps, GetStaticPaths } from "next";
-import BackArrow from "@/components/icons/backArrow";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 export default function Post({
   postData,
@@ -13,10 +14,11 @@ export default function Post({
     contentHtml: string;
   };
 }) {
+  const title = postData.title + " - Vedant Grover";
   return (
     <>
       <Head>
-        <title>{postData.title} - Vedant Grover</title>
+        <title>{title}</title>
       </Head>
       <div className="sm:px-8 mt-16 lg:mt-32">
         <div className="mx-auto w-full max-w-7xl lg:px-8">
@@ -28,9 +30,12 @@ export default function Post({
                     type="button"
                     href="/blogs"
                     aria-label="Go back to blog articles"
-                    className="group mb-8 flex h-10 w-10 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 transition dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0 dark:ring-white/10 dark:hover:border-zinc-700 dark:hover:ring-white/20 lg:absolute lg:-left-5 lg:-mt-2 lg:mg-0 xl:-top-1.5 xl:left-0 xl:mt-0"
+                    className="group mb-8 flex h-10 w-10 items-center justify-center rounded-full select-none bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0 dark:ring-white/10 dark:hover:border-zinc-700 dark:hover:ring-white/20 lg:absolute lg:-left-5 lg:-mt-2 lg:mg-0 xl:-top-1.5 xl:left-0 xl:mt-0"
                   >
-                    <BackArrow />
+                    <FontAwesomeIcon
+                      icon={faArrowRight}
+                      className="text-zinc-500 group-hover:text-zinc-700 dark:text-zinc-500 dark:group-hover:text-zinc-400 rotate-180"
+                    />
                   </a>
                   <article>
                     <header className="flex flex-col">
@@ -47,7 +52,7 @@ export default function Post({
                         </span>
                       </time>
                     </header>
-                    <div className="mt-8 prose dark:prose-invert prose-img:mx-auto prose-img:w-full prose-img:h-auto prose-img:rounded-[36px] prose-img:object-cover prose-img:aspect-auto">
+                    <div className="mt-8 prose dark:prose-invert prose-img:mx-auto prose-img:w-full prose-img:h-auto prose-img:rounded-[36px] prose-img:object-cover prose-img:aspect-auto prose-img:select-none prose-a:no-underline prose-cyan">
                       <div
                         dangerouslySetInnerHTML={{
                           __html: postData.contentHtml,
