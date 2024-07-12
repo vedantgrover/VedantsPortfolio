b71e0ec96ab31ff45b7b0cce2de5c8b0a24457a8
vedantgrover <68160833+vedantgrover@users.noreply.github.com>
2024-07-11 22:20:46 -0700

    Adds a width to keep the text wrapped and visible

--- app/components/CatchySloganBox.tsx
+++ app/components/CatchySloganBox.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const CatchySloganBox = memo(() => {
     return (
-        <div className="mt-8">
+        <div className="mt-8 w-[87%]">
             <h1 className="text-yellow-500 text-xl font-bold">Making a positive dent in the universe, one pixel at a time.</h1>
             <p className="mt-2 text-yellow-500 italic">I aspire to be a Software Engineer, working to create innovative solutions to everyday problems.</p>
         </div>
