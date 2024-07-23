9a152e41ff1bf7eb37c05d567c034a3ef7d8c913
vedantgrover <68160833+vedantgrover@users.noreply.github.com>
2024-07-23 15:55:58 -0700

    Adds a Marquee to scroll across the background

--- app/sections/HomeSection.tsx
+++ app/sections/HomeSection.tsx
@@ -1,10 +1,14 @@
 import {memo} from "react";
 import Greeting from "@/app/components/Greeting";
+import MarqueeText from "@/app/components/MarqueeText";
 
 const HomeSection = memo(() => {
     return (
         <section id="home" className="flex justify-center items-center border h-screen">
-            <div className="">
+            <div className="absolute overflow-x-hidden">
+                <MarqueeText />
+            </div>
+            <div>
                 <Greeting/>
             </div>
         </section>
