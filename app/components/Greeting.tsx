657904a414b1c0144a8085140ae659a252a4a152
vedantgrover <68160833+vedantgrover@users.noreply.github.com>
2024-07-23 11:39:26 -0700

    Replaces padding with space for more efficient gaps between each element

--- app/components/Greeting.tsx
+++ app/components/Greeting.tsx
@@ -4,8 +4,8 @@ import ImageOfMe from "@/app/components/ImageOfMe";
 
 const Greeting = memo(() => {
     return (
-        <div className="flex flex-col items-center justify-between">
-            <h1 className="text-5xl font-[500] tracking-wider pb-2">Hi, I&apos;m <b>Vedant!</b></h1>
+        <div className="flex flex-col items-center justify-between space-y-4">
+            <h1 className="text-5xl font-[500] tracking-wider">Hi, I&apos;m <b>Vedant!</b></h1>
             <ChangingRoleText/>
             <ImageOfMe />
         </div>
