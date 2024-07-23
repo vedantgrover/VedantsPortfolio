a83f8ed7bf56eb8c1082f9b96c104902f1871119
vedantgrover <68160833+vedantgrover@users.noreply.github.com>
2024-07-23 12:10:37 -0700

    Makes image bigger

--- app/components/ImageOfMe.tsx
+++ app/components/ImageOfMe.tsx
@@ -1,11 +1,13 @@
+'use client'
+
 import {memo} from "react";
 import Image from "next/image";
 
 const ImageOfMe = memo(() => {
     return (
         <div
-            className="h-[280px] w-[280px] overflow-hidden z-[1] rounded-[48px] justify-center shadow-md">
-            <Image src={"/me.jpg"} alt={"Vedant"} width={280} height={250} className="relative -top-14" />
+            className="h-[300px] w-[300px] overflow-hidden z-[1] rounded-[48px] justify-center shadow-md">
+            <Image src={"/me.jpg"} alt={"Vedant"} width={300} height={300} className="relative -top-14 mx-auto" />
         </div>
     )
 })
