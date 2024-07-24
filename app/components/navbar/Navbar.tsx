493bc87df73ecb35539268644fe0e747f81f5ce6
vedantgrover <68160833+vedantgrover@users.noreply.github.com>
2024-07-24 12:57:20 -0700

    Makes navbar the highest in stack

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = memo(({links}: NavbarProps) => {
     const activeSection = useSectionObserver(sectionIds)
 
     return (
-        <nav className="flex justify-center items-end backdrop-blur-[10px] fixed w-full h-[75px]">
+        <nav className="flex justify-center items-end backdrop-blur-[10px] fixed w-full h-[75px] z-[1000]">
             <div className="bg-white rounded-3xl flex flex-row shadow-md">
                 {links.map((l) => <NavbarItem key={l.title} selected={activeSection === l.id} title={l.title}
                                               link={l.id}/>)}
