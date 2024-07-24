d4945ed39ceb2ccdca7c6508397af428d6466a84
vedantgrover <68160833+vedantgrover@users.noreply.github.com>
2024-07-24 13:06:58 -0700

    Makes highlighted background slide through all options

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
     return (
         <div className="flex flex-col justify-between">
             <HomeSection/>
+            <section id="about" className="h-screen">About Me</section>
         </div>
     );
 }
