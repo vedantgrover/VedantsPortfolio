e325a77c5e797dfdeb4eac5cf3cf810b6fb67b8b
vedantgrover <68160833+vedantgrover@users.noreply.github.com>
2024-07-24 11:59:00 -0700

    Removes height constraint from website container and adds it to sections

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import HomeSection from "@/app/sections/HomeSection";
 
 export default function Home() {
     return (
-        <div className="h-screen flex flex-col justify-between">
+        <div className="flex flex-col justify-between">
             <HomeSection/>
         </div>
     );
