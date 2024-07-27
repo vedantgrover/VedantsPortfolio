312d12d4aab6d79af8bd974a5b8d14e9402f21db
vedantgrover <68160833+vedantgrover@users.noreply.github.com>
2024-07-27 15:43:32 -0700

    Removes Marquee text effect

--- app/sections/HomeSection.tsx
+++ app/sections/HomeSection.tsx
@@ -1,13 +1,9 @@
 import {memo} from "react";
 import Greeting from "@/app/components/Greeting";
-import MarqueeText from "@/app/components/MarqueeText";
 
 const HomeSection = memo(() => {
     return (
         <section id="home" className="flex justify-center items-center border h-screen">
-            <div className="absolute overflow-x-hidden">
-                <MarqueeText />
-            </div>
             <div>
                 <Greeting/>
             </div>
