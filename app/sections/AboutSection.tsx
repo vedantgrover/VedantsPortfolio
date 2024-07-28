8cc53c6f56b18e8e92c09d3198a27e14e688c908
vedantgrover <68160833+vedantgrover@users.noreply.github.com>
2024-07-27 22:29:23 -0700

    Adds images to side section

--- app/sections/AboutSection.tsx
+++ app/sections/AboutSection.tsx
@@ -1,17 +1,35 @@
-import {memo} from "react";
+'use client'
+
+import {memo, useRef} from "react";
 import AboutMeText from "@/app/components/AboutMeText";
+import Image from "next/image";
+import {useScroll, useTransform} from "framer-motion";
 
 const AboutSection = memo(() => {
     const aboutMeDetails: { title: string, description: string, image: string }[] = [
         {
             title: "FIRST Robotics",
             description: "I was a part of FRC Team 2910 as a Software Developer, Software Lead, Team Representative, and Data Analyzer.",
-            image: ""
+            image: "/me1.jpg"
+        }, {
+            title: "FIRST Robotics",
+            description: "I was a part of FRC Team 2910 as a Software Developer, Software Lead, Team Representative, and Data Analyzer.",
+            image: "/me.jpg"
         }
     ]
 
+    const targetRef = useRef(null);
+
+    const {scrollYProgress} = useScroll({container: targetRef, offset: ["start start", "end end"]})
+
+    const yTransforms = aboutMeDetails.map((_, index) =>
+        useTransform(scrollYProgress, [index / aboutMeDetails.length, (index + 1) / aboutMeDetails.length], ["0%", "-100%"])
+    )
+
+
     return (
-        <section id="about" style={{ height: `${aboutMeDetails.length * 100}vh`}} className="mt-0 static flex">
+        <section id="about" style={{height: `${aboutMeDetails.length * 100}vh`}} className="mt-0 static flex"
+                 ref={targetRef}>
             <div className="relative overflow-hidden w-1/2 h-full">
                 <div className="static w-full h-full">
                     {aboutMeDetails.map(detail => (
@@ -19,7 +37,21 @@ const AboutSection = memo(() => {
                     ))}
                 </div>
             </div>
-            <div className="float-none absolute overflow-visible box-border flex w-1/2 left-1/2 content-center items-center h-screen"></div>
+            <div className="sticky top-0 flex w-1/2 left-1/2 content-center items-center h-screen">
+                <div className="w-[31vw] h-[31vw] flex overflow-hidden rounded-[11%] z-[100]">
+                    {aboutMeDetails.map((detail, index) => (
+                        <Image
+                            key={index}
+                            src={detail.image}
+                            alt={detail.title}
+                            width={500}
+                            height={500}
+                            objectFit="cover"
+                            className="object-cover"
+                        />
+                    ))}
+                </div>
+            </div>
         </section>
     )
 })
