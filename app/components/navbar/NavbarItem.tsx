5022bad39da739a40d9cab1697a56c24a3bdfbe2
vedantgrover <68160833+vedantgrover@users.noreply.github.com>
2024-08-04 13:31:53 -0700

    Makes animation more stable for NavbarItem

--- app/components/navbar/NavbarItem.tsx
+++ app/components/navbar/NavbarItem.tsx
@@ -10,13 +10,18 @@ interface NavbarItemProps {
 
 const NavbarItem = memo(({selected, title, link}: NavbarItemProps) => {
     return (
-        <Link href={`#${link}`} className={`relative py-[10px] px-[18px] text-center flex`}>
-            {selected &&
-                <motion.div layoutId="activeBackground" className="absolute inset-0 bg-green-900 rounded-[96px] z-0"
-                            initial={false} transition={{type: "spring", stiffness: 300, damping: 30}}/>}
-            <motion.div whileHover={selected ? {translateY: 0} : {translateY: -5}}
-                        className={`${selected ? "text-white font-bold" : "text-black font-normal"} relative z-10 text-[14px]`}>
-                {title}
+        <Link href={`#${link}`} className="relative py-[10px] px-[18px] text-center flex">
+            <motion.div initial="initial" whileHover="hovered">
+                {selected &&
+                    <motion.div layoutId="activeBackground" className="absolute inset-0 bg-green-900 rounded-[96px] z-0"
+                                initial={false} transition={{type: "spring", stiffness: 300, damping: 30}}/>}
+                <motion.div variants={{
+                    initial: {y: 0},
+                    hovered: {y: selected ? 0 : -5}
+                }}
+                            className={`${selected ? "text-white font-bold" : "text-black font-normal"} relative z-10 text-[14px]`}>
+                    {title}
+                </motion.div>
             </motion.div>
         </Link>
     )
