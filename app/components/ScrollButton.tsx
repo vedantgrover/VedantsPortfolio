5e06b5e91225ecd5de6ce3093c4b0d5fa3d8fb8d
Vedant Grover <vedantvgrover@gmail.com>
2025-03-14 14:56:39 -0400

    Makes scroll arrow point downward

--- app/components/ScrollButton.tsx
+++ app/components/ScrollButton.tsx
@@ -1,11 +1,12 @@
 import { memo } from "react";
 import DownArrow from "./icons/DownArrow";
+import Link from "next/link";
 
 const ScrollButton = memo(() => {
   return (
-    <div className="h-min w-min dark:border-white/80 border-[#333333] rounded-full border-dashed p-1 border-2 animate-bounce">
+    <Link href="/#about" className="h-min w-min dark:border-white/80 border-[#333333] rounded-full border-dashed p-1 border-2 animate-bounce">
       <DownArrow />
-    </div>
+    </Link>
   );
 });
 
