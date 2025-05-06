134a20b33e67d30cedd03bd1d430ed62d319aa98
Vedant Grover <vedantvgrover@gmail.com>
2025-05-05 20:38:09 -0700

    Remove scroll down button on mobile devices

--- app/sections/HomeSection.tsx
+++ app/sections/HomeSection.tsx
@@ -9,17 +9,17 @@ const HomeSection: React.FC = memo(() => {
   return (
     <motion.section
       id="home"
-      className="flex flex-col justify-center items-center max-h-screen h-screen"
+      className="relative flex flex-col items-center justify-center h-screen w-full"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.5 }}
       aria-label="Home Section"
     >
-      <div>
+      <div className="flex flex-col items-center justify-center w-full h-full">
         <Greeting />
       </div>
       <motion.div 
-        className="xs:hidden md:flex absolute bottom-10"
+        className="hidden md:flex absolute bottom-10"
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ delay: 0.5, duration: 0.5 }}
